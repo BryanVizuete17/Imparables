19312fc9f2509a5435155134f3ce5903ff3ea2ed
BryanVizuete17 <45468285+BryanVizuete17@users.noreply.github.com>
2020-08-20 21:43:31 -0500

    creacion de la pantalla informacion.js

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Informacion} from './screens/Informacion';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/Entypo';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function TabHome(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <Tab.Navigator>
+      <Tab.Screen 
+      name="InformacionScreen"
+         component={Informacion}
+         options={{
+          tabBarLabel:"Informacion",
+          tabBarIcon:()=>(
+          <Icon
+          name='user'
+          size={32}
+          color="skyblue"/>
+        )      
+       }}
+       ></Tab.Screen>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    </Tab.Navigator>
+  )
+  
+  }
+  
+  export default function App() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="TabHomeScreen">
+          <Stack.Screen name="TabHomeScreen" component={TabHome}></Stack.Screen>
+          <Stack.Screen name="InformacionScreen" component={Informacion}></Stack.Screen>
+          
+          
+          
+          </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
---
+++ screens/Informacion.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Alert, Button, FlatList } from 'react-native';
+
+export class Informacion extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Informacion</Text>
+            </View>
+        )
+    }
+
+}
